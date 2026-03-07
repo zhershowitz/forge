d6d79ddd7e84326d71b24ca043fed93f8a095566
Zachary Hershowitz <zhershowitz99@gmail.com>
2026-03-07 00:14:34 -0500

    Update forgeItemSheet.js
    can't figure out why it won't work like in the guide

--- modules/sheets/forgeItemSheet.js
+++ modules/sheets/forgeItemSheet.js
@@ -1,5 +1,5 @@
 export default class forgeItemSheet extends ItemSheet {
 get template() {
-    return `systems/forge/templates/sheets/${this.item.data.type}-sheet.hbs`;
+    return `systems/forge/templates/sheets/${this.item.data.type}-sheet.html`;
 }
 }
